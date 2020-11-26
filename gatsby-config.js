34e9246ff9614de1ed1210b2eede12833be2a835
lyfar <egor.lyfar@gmail.com>
2020-11-26 11:02:50 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: 'http://zfx.local/graphql',
+        baseUrl: 'http://012ada84a626.ngrok.io/graphql',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
         // The protocol. This can be http or https.
